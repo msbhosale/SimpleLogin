6bfcf117997c25fb419da3c6d452ed3f3f3fea02
MS Bhosale <msbhosale97@gmail.com>
2018-12-31 18:03:10 +0530

    Added JDBC code

--- src/com/msbhosale/daos/UserDAO.java
+++ src/com/msbhosale/daos/UserDAO.java
@@ -1,12 +1,39 @@
 package com.msbhosale.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.msbhosale.settings.JDBC;
+
 public class UserDAO {
 
 	public boolean isValidUser(String username, String password) {
 
 		boolean status = false;
+		int numberOfRecords = 0;
 
-		if (username.equals("admin") && password.equals("1234")) {
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			Connection con = JDBC.getConnection();
+			PreparedStatement pst = con.prepareStatement("SELECT * FROM user WHERE username=? and password=?");
+
+			pst.setString(1, username);
+			pst.setString(2, password);
+
+			ResultSet rs = pst.executeQuery();
+
+			while (rs.next()) {
+				numberOfRecords++;
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		if (numberOfRecords > 0) {
 			status = true;
 		}
 
